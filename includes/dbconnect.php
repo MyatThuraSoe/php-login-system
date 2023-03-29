1c073649c663d966f4f92b8cd39eb3eeccedaa8f
Myat Thura Soe <89846206+MyatThuraSoe@users.noreply.github.com>
2023-03-29 22:57:44 +0630

    Update dbconnect.php

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -4,10 +4,7 @@
 // $dbPassword = "";
 // $dbName = "login_authentication";
 
-// $dbServer = "sql210.epizy.com"; -- infinity free
-// $dbUsername = "epiz_33897234";
-// $dbPassword = "h95HtTQtbAdu";
-// $dbName = "epiz_33897234_login_authentication";
+
 
 // Looing for .env at the root directory
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
